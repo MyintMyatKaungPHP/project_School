6260d906837c5610cb781e98ea06883f22437014
MyintMyatKaungPHP <myintmyatkaung.info@gmail.com>
2018-12-06 08:15:06 +0630

    All student UI

---
+++ resources/views/dashboard/layouts/error.blade.php
@@ -0,0 +1,13 @@
+@if ($errors->any())
+    <div class="alert alert-danger text-center">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+@if(session()->has('status'))
+    <p class="alert alert-success text-center">{{session('status')}}</p>
+@endif
